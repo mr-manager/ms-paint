e9056a2742b6c87c878a7b2780893112589f5805
Jonas Pickholtz <jpickhol@gmail.com>
2019-05-31 16:30:13 +0300

    added functional color palette

--- JS/paint.js
+++ JS/paint.js
@@ -1,18 +1,28 @@
 var canvas = document.getElementById("canvas");
 var isDrawing = false;
+var colorInUse = "black";
 
-canvas.addEventListener("mousedown", function() {
+var colorButton = document.getElementsByClassName("colors-btn");
+for(var i = 0; i < colorButton.length; i++) {
+    colorButton[i].addEventListener("click", function(e) {
+        var clickedColor = e.target;
+        colorInUse = clickedColor.id;
+        console.log(colorInUse);
+    });
+};
+
+canvas.addEventListener("mousedown", function () {
     isDrawing = true;
     keepDrawing();
-});
+})
 
 function keepDrawing() {
     canvas.addEventListener("mousemove", draw)
 }
 
-canvas.addEventListener("mouseup", function() {
+canvas.addEventListener("mouseup", function () {
     isDrawing = false;
-});
+})
 
 function draw(e) {
     if (isDrawing === true) {
@@ -22,10 +32,10 @@ function draw(e) {
         console.log(coordinates);
 
         var appendDiv = document.createElement("div");
-        appendDiv.classList = "black";
+        appendDiv.classList = colorInUse;
         canvas.appendChild(appendDiv);
         appendDiv.style.top = e.clientY + "px";
         appendDiv.style.left = e.clientX + "px";
     }
-}
+};
 
