69a95680453456355b97de6825f200fe9ea6c6e5
Jonas Pickholtz <jpickhol@gmail.com>
2019-06-01 18:22:12 +0300

    added hover effect, clear page button and cleaned up code

--- JS/paint.js
+++ JS/paint.js
@@ -4,56 +4,63 @@ var colorInUse = "black";
 var brushSizeInUse = "height: 5px; width: 5px";
 
 var colorButton = document.getElementsByClassName("colors-btn");
-for(var i = 0; i < colorButton.length; i++) {
-    colorButton[i].addEventListener("click", function(e) {
+for (var i = 0; i < colorButton.length; i++) {
+    colorButton[i].addEventListener("click", function (e) {
         var clickedColor = e.target;
         colorInUse = clickedColor.id;
-        console.log(colorInUse);
     })
 };
 
 var brushSizeButtons = document.getElementsByClassName("brushsize-btn");
 for (var i = 0; i < brushSizeButtons.length; i++) {
-    brushSizeButtons[i].addEventListener("click", function(e){
+    brushSizeButtons[i].addEventListener("click", function (e) {
         var clickedBrushSize = e.target;
         brushSizeInUse = clickedBrushSize.id;
-        if(brushSizeInUse === "large") {
+        if (brushSizeInUse === "large") {
             brushSizeInUse = "height: 10px; width: 10px";
-        } else if(brushSizeInUse === "extra-large"){
+        } else if (brushSizeInUse === "extra-large") {
             brushSizeInUse = "height: 20px; width: 20px";
         } else {
             brushSizeInUse = "height: 5px; width: 5px";
         }
-        console.log(brushSizeInUse);
     })
 };
 
 canvas.addEventListener("mousedown", function () {
     isDrawing = true;
     keepDrawing();
-})
+});
 
 function keepDrawing() {
     canvas.addEventListener("mousemove", draw)
-}
+};
 
 canvas.addEventListener("mouseup", function () {
     isDrawing = false;
-})
+});
 
 function draw(e) {
     if (isDrawing === true) {
-        var x = e.clientX;
+        /*var x = e.clientX;
         var y = e.clientY;
         var coordinates = `X coordinates are ${x} and Y coordinates are ${y}`
-        console.log(coordinates);
+        console.log(coordinates);*/
 
         var appendDiv = document.createElement("div");
         appendDiv.classList = colorInUse;
         appendDiv.style = brushSizeInUse;
+        appendDiv.classList.add('drawn');
         canvas.appendChild(appendDiv);
         appendDiv.style.top = e.clientY + "px";
         appendDiv.style.left = e.clientX + "px";
     }
 };
 
+var clearScreenButton = document.getElementById("clear-screen");
+clearScreenButton.addEventListener("click", clearTheScreen);
+function clearTheScreen() {
+    var drawing = document.getElementsByClassName("drawn");
+    while (drawing.length > 0) {
+        canvas.removeChild(drawing[0]);
+    }
+};
